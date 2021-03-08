a99e56c0ad92f251dda87b775655d4c28403950b
robbobby <69080884+robbobby@users.noreply.github.com>
2021-03-08 10:08:51 +0000

    Updated

--- start-installs.sh
+++ start-installs.sh
@@ -1,2 +1,9 @@
+#!/bin/bash
+#date: 08/03/2021
+#author: Robert Hollingworth
+#purpose: AWS EC2 Instance initial server installs,:
+    # gcc compiler
+
+
 sudo su
-yum group install "Development Tools"
+yum -y group install "Development Tools"
